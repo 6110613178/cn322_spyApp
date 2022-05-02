458d04731ce6d5481413a719932bcf1e32825452
Tatar <tatar5599@gmail.com>
2022-05-03 00:01:37 +0700

    Commit 2

--- chartamue/spy/views.py
+++ chartamue/spy/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import *
 from django.contrib.auth import login, authenticate,logout
-from django.http import HttpResponseRedirect , HttpResponse
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib import messages
@@ -18,7 +18,7 @@ def LoginPage(request):
             return redirect('Adminhomepage')  
         elif request.user.userprofile.role == 'Spy' :
             request.session["role"] = 'Spy'
-            return redirect('SpyHomepage')
+            return redirect('Spyhomepage')
     return render(request,"login.html")
 
 
@@ -54,22 +54,38 @@ def Logout(request):
 def MissionDetail(request , mission_id) :
     if request.user.is_authenticated :
         mission = Mission.objects.get(id=mission_id)
-        userprofile = mission.spy.userprofile
+        if request.user.userprofile.role =="Admin":
+            userprofile = mission.spy.userprofile
+        else :
+            if request.user.userprofile != mission.spy.userprofile:
+                return HttpResponseRedirect(reverse('LoginPage'))
+            userprofile = request.user.userprofile
         rsa_key = userprofile.rsa_key.getValueInt()
         privateKey = rsa.PrivateKey(n=rsa_key["n"],e=rsa_key["e"],d=rsa_key["d"],p = rsa_key["p"] ,q= rsa_key["q"])
+
         name_decrypt = rsa.decrypt(mission.mission_name, privateKey).decode()
         descriptions_decrypt = rsa.decrypt(mission.mission_descriptions, privateKey).decode()
+        date = mission.date_start
+        status = mission.status
+
         if request.method == "POST" :
-            print(request.POST)
             status = request.POST['status']
             mission.status = status
             mission.save()
 
+            on_going = Mission.objects.filter(spy__userprofile=userprofile , status='on going').count()
+            userprofile.ongoing_mission = on_going
+            waiting =  Mission.objects.filter(spy__userprofile=userprofile , status='waiting').count()
+            userprofile.waiting_mission = waiting
+            complete =  Mission.objects.filter(spy__userprofile=userprofile , status='complete').count()
+            userprofile.complete_mission = complete
+            userprofile.save()
+
         return render(request , "mission_detail.html" ,  {   "id": mission.id,
                     "mission_name": name_decrypt,
                     "mission_descriptions" : descriptions_decrypt,
-                    "date_start": mission.date_start,
-                    "status": mission.status
+                    "date_start": date,
+                    "status": status
                  })
 
     return redirect('/')
@@ -102,7 +118,6 @@ def SpyProfile(request,id):
 def DeleteSpyProfile(request,id):
     if request.user.is_authenticated :
         if request.method == 'POST':
-            print("delete")
             delete_user = User.objects.get(id=id)
             delete_user.delete()
             return HttpResponseRedirect(reverse('Adminhomepage'))
@@ -111,39 +126,54 @@ def DeleteSpyProfile(request,id):
 
 def SpyHomepage(request):
     if request.user.is_authenticated :
-        user_profile = request.user.userprofile
+        userprofile = request.user.userprofile
+        rsa_key = userprofile.rsa_key.getValueInt()
+        privateKey = rsa.PrivateKey(n=rsa_key["n"],e=rsa_key["e"],d=rsa_key["d"],p = rsa_key["p"] ,q= rsa_key["q"])
+        missionDecrypt= []
         missions = Mission.objects.filter(spy=request.user).order_by('date_start')
-        return render(request,"spy_homepage.html" , {
-            'missions' : missions,
-            'total' : user_profile.complete_mission + user_profile.ongoing_mission + user_profile.waiting_mission
-        })
-    return HttpResponseRedirect(reverse('LoginPage'))
-    #return redirect('/')
+        for mission in missions:
+            name_decrypt = rsa.decrypt(mission.mission_name, privateKey).decode()
+            descriptions_decrypt = rsa.decrypt(mission.mission_descriptions, privateKey).decode()
+            missionDecrypt.append(
+                {   "id": mission.id,
+                    "mission_name": name_decrypt,
+                    "mission_descriptions" : descriptions_decrypt,
+                    "date_start": mission.date_start,
+                    "status": mission.status
+                 }
+            )
 
+        return render(request,"spy_homepage.html" , {
+            'missions' : missionDecrypt,
+            'total' : userprofile.complete_mission + userprofile.ongoing_mission + userprofile.waiting_mission
+        })
+
+    return HttpResponseRedirect(reverse('LoginPage'))
 
 def AddSpy(request):
     if request.user.is_authenticated  and request.user.userprofile.role == 'Admin' :
         if request.method == 'POST':
             code_name = request.POST.get('code_name')
-            password = request.POST.get('username')
-            print(password)
-            print(code_name)
-            user = User.objects.create_user(username=code_name , password=password)
+            username = request.POST.get('username')
+            user = User.objects.create_user(username=username , password=username)
             n,e,d,p,q = generateKey()
             rsa_key = RSAKey(n=n,e=e,d=d,p=p,q=q)
             rsa_key.save()
             new_user = UserProfile(user = user,role='Spy',code_name=code_name,rsa_key = rsa_key)
             new_user.save()
-            return HttpResponseRedirect(reverse('Adminhomepage'))
+            return HttpResponseRedirect(reverse('LoginPage'))
         return render(request,"add_spy.html")
     return redirect('/')
     
 def EditPassword(request):
+    
     form = PasswordChangeForm(request.user , request.POST or None)
 
     if form.is_valid() :
 
         form.save()
+
+        return HttpResponseRedirect(reverse('Spyhomepage'))
 
     return render(request,"Edit_password.html" , {
         'form' : form
@@ -152,11 +182,12 @@ def EditPassword(request):
 def Adminhomepage(request):
     if request.user.is_authenticated :
         all_spy = UserProfile.objects.filter(role = "Spy")
-        
+        missions = Mission.objects.all()
             
         return render(request,"admin_homepage.html",
         {
-            'all_spy' : all_spy
+            'all_spy' : all_spy,
+            'missions' : missions
         })
     return redirect('/')
 
@@ -168,29 +199,16 @@ def AddMission(request):
             missionDescriptions = request.POST['mission_detail']
             date = request.POST['dateMission']
             status = "on going"
-            spyID = request.POST['spy_mission_owner'] #'9'
-
-            userProfile = UserProfile.objects.filter(id = spyID)[0] #Object UserProfile
-            rsa_key = userProfile.rsa_key.getValueInt()
-
+            spyID = request.POST['spy_mission_owner'] 
+            userProfile = UserProfile.objects.filter(id = spyID) #Object UserProfile
+            rsa_key = userProfile[0].rsa_key.getValueInt()
             publicKey_spy = rsa.PublicKey(n=rsa_key["n"],e=rsa_key["e"])
-            admin = UserProfile.objects.filter(role = "Admin")[0]
-            rsa_key_admin = admin.rsa_key.getValueInt()
-            publicKey_admin = rsa.PublicKey(n=rsa_key_admin["n"],e= rsa_key_admin["e"])
-
             encMissionNameSpy = rsa.encrypt(missionName.encode(),publicKey_spy)
-            encMissionNameAdmin = rsa.encrypt(missionName.encode(),publicKey_admin)
-
-            print(encMissionNameSpy)
-                
             encMissionDesSpy = rsa.encrypt(missionDescriptions.encode(),publicKey_spy)
-            encMissionDesAdmin = rsa.encrypt(missionDescriptions.encode(),publicKey_admin)
-
-            user = userProfile.user #Object User
+            user = userProfile[0].user #Object User
             mission = Mission(mission_name=encMissionNameSpy,mission_descriptions=encMissionDesSpy,date_start=date,status=status,spy=user)
             mission.save()
-            missionAdd = MissionAdmin(mission_name=encMissionNameAdmin,mission_descriptions=encMissionDesAdmin,date_start=date,status=status,spy=user,mission=mission)
-            missionAdd.save()
+            userProfile.update(ongoing_mission = userProfile[0].ongoing_mission + 1)
         else:
             all_spy = UserProfile.objects.filter(role='Spy')
             return render(request,"add_mission.html",{'all_spy': all_spy})
